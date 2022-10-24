5d72dd2218b4bd2c2d11ebf626a072bbbed1d86b
vik37 <59177404+vik37@users.noreply.github.com>
2022-10-24 16:49:37 +0200

    Base service interface and implementation. Api Request.

---
+++ Mango.Web/Models/ApiRequest.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using static Mango.Web.SD;
+
+namespace Mango.Web.Models
+{
+    public class ApiRequest
+    {
+        public ApiType ApiType { get; set; } = ApiType.GET;
+        public string Url { get; set; }
+        public object Data { get; set; }
+        public string Token { get; set; }
+    }
+}
