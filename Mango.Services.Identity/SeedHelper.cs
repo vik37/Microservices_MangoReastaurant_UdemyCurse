c616c9eb33a295e07ebe69d0567a95599f9d647d
vik37 <59177404+vik37@users.noreply.github.com>
2022-10-26 12:36:21 +0200

    Refactor Seed method. Create on separate place.

---
+++ Mango.Services.Identity/SeedHelper.cs
@@ -0,0 +1,23 @@
+﻿using Mango.Services.Identity.DbContexts;
+using Mango.Services.Identity.DbContexts.Initializer;
+using Mango.Services.Identity.Models;
+using Microsoft.AspNetCore.Identity;
+
+namespace Mango.Services.Identity
+{
+    public static class SeedHelper
+    {
+        public static void SeedDb(WebApplication app)
+        {
+            var context = app.Services.CreateScope().ServiceProvider.GetService<ApplicationDbContext>();
+
+            var userManager = app.Services.CreateScope().ServiceProvider.GetService<UserManager<ApplicationUser>>();
+
+            var roleManager = app.Services.CreateScope().ServiceProvider.GetService<RoleManager<IdentityRole>>();
+
+            var dbInitializer = new DbInitializer(context, userManager, roleManager);
+
+            dbInitializer.Initialize();
+        }
+    }
+}
