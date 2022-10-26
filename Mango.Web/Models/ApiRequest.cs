ccbeb79eeb6189a41ab365b9aa467fd9e2aacfcd
vik37 <59177404+vik37@users.noreply.github.com>
2022-10-26 21:47:24 +0200

    Finish user protection.

--- Mango.Web/Models/ApiRequest.cs
+++ Mango.Web/Models/ApiRequest.cs
@@ -8,6 +8,6 @@ namespace Mango.Web.Models
         public ApiType ApiType { get; set; } = ApiType.GET;
         public string Url { get; set; }
         public object Data { get; set; }
-        public string Token { get; set; }
+        public string AccessToken { get; set; }
     }
 }
