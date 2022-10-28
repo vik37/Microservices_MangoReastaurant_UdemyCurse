0d8b9893090a4bd9210d1736dd920fed4f2bef78
vik37 <59177404+vik37@users.noreply.github.com>
2022-10-28 20:42:52 +0200

    Prepare the Coupon API Service.

---
+++ Mango.Services.CouponAPI/MappingConfig.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+
+namespace Mango.Services.ShoppingCartAPI
+{
+    public class MappingConfig
+    {
+        public static MapperConfiguration RegisterMapp()
+        {
+            var mapppingConfig = new MapperConfiguration(config =>
+            {
+
+            });
+            return mapppingConfig;
+        }
+    }
+}
