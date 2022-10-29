bd7d2fd1658b388367a9ea8ad44ae744574a5cbf
vik37 <59177404+vik37@users.noreply.github.com>
2022-10-29 18:56:47 +0200

    Checkout Functionality.

--- Mango.Web/Controllers/CartController.cs
+++ Mango.Web/Controllers/CartController.cs
@@ -24,6 +24,31 @@ namespace Mango.Web.Controllers
         {
             return View(await LoadCartDtoBasedOnLoggedInUser());
         }
+        [HttpGet]
+        public async Task<IActionResult> Checkout()
+        {
+            return View(await LoadCartDtoBasedOnLoggedInUser());
+        }
+        [HttpPost]
+        public async Task<IActionResult> Checkout(CartDto cartDto)
+        {
+            try
+            {
+                var accessToken = await HttpContext.GetTokenAsync("access_token");
+                var response = await _cartService.Checkout<ResponseDto>(cartDto.CartHeader, accessToken);
+                return RedirectToAction(nameof(Confirmation));
+            }
+            catch (Exception ex)
+            {
+
+                return View(cartDto);
+            }
+        }
+        [HttpGet]
+        public async Task<IActionResult> Confirmation()
+        {
+            return View();
+        }
         public async Task<CartDto> LoadCartDtoBasedOnLoggedInUser()
         {
             var userId = User.Claims.Where(u => u.Type == "sub")?.FirstOrDefault().Value;
@@ -87,10 +112,6 @@ namespace Mango.Web.Controllers
                 return RedirectToAction(nameof(CartIndex));
             return View();
         }
-        [HttpGet]
-        public async Task<IActionResult> Checkout()
-        {
-            return View(await LoadCartDtoBasedOnLoggedInUser());
-        }
+        
     }
 }
