4f62c034c4c2161bd47e2c92a7787c4e2c8d13bf
vik37 <59177404+vik37@users.noreply.github.com>
2022-10-30 19:53:05 +0100

    Order API add Models, Required Nuget Packages, Migrations, Configurations, Repositories.

---
+++ Mango.Services.OrderAPI/MappingConfig.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+
+namespace Mango.Services.OrderAPI
+{
+    public class MappingConfig
+    {
+        public static MapperConfiguration RegisterMapp()
+        {
+            var mapppingConfig = new MapperConfiguration(config =>
+            {
+
+            });
+            return mapppingConfig;
+        }
+    }
+}
---
+++ Mango.Services.OrderAPI/Program.cs
@@ -0,0 +1,97 @@
+using AutoMapper;
+using Mango.Services.OrderAPI;
+using Mango.Services.OrderAPI.DbContexts;
+using Mango.Services.OrderAPI.Repository;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.Net.Http.Headers;
+using Microsoft.OpenApi.Models;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+IConfiguration configuration = builder.Configuration;
+var services = builder.Services;
+string identityUrl = configuration["ServiceUrls:IdentityService"];
+
+// Add services to the container.
+services.AddDbContext<ApplicationDbContext>(opt =>
+                    opt.UseSqlServer(configuration.GetConnectionString("DefaultConnection")));
+
+IMapper mapper = MappingConfig.RegisterMapp().CreateMapper();
+services.AddSingleton(mapper);
+services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+
+var optionBuilder = new DbContextOptionsBuilder<ApplicationDbContext>();
+optionBuilder.UseSqlServer(configuration.GetConnectionString("DefaultConnection"));
+services.AddSingleton(new OrderRepository(optionBuilder.Options));
+
+services.AddControllers();
+services.AddEndpointsApiExplorer();
+
+services.AddAuthentication("Bearer")
+    .AddJwtBearer("Bearer", opt =>
+    {
+        opt.Authority = identityUrl;
+        opt.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateAudience = false
+        };
+    });
+services.AddAuthorization(opt =>
+{
+    opt.AddPolicy("ApiScope", policy =>
+    {
+        policy.RequireAuthenticatedUser();
+        policy.RequireClaim("scope", "mango");
+    });
+});
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+services.AddSwaggerGen(c =>
+{
+    c.EnableAnnotations();
+    c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+    {
+        Description = @"Enter 'Bearer' [space] and your token",
+        Name = HeaderNames.Authorization,
+        In = ParameterLocation.Header, // or magic string "Authorization"
+        Type = SecuritySchemeType.ApiKey,
+        Scheme = "Bearer",
+    });
+    c.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Type = ReferenceType.SecurityScheme,
+                    Id = "Bearer"
+                },
+                Scheme = "oauth2",
+                Name = "Bearer",
+                In = ParameterLocation.Header
+            },
+            new List<string>()
+        }
+    });
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+app.UseRouting();
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
